856e28b47132b574255dbb12272657bf2c619f85
Ward Huang <ward.huang@sjsu.edu>
2021-01-25 18:16:43 -0800

    format js, html

--- web/app.js
+++ web/app.js
@@ -3,12 +3,12 @@
  * Bestbuy autocomplete
  *
  */
- 'use strict';
+"use strict";
 
-const http = require('http');
-const path = require('path');
-const express = require('express');
-const { Client } = require('@elastic/elasticsearch');
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const { Client } = require("@elastic/elasticsearch");
 
 const app = express();
 
@@ -16,44 +16,54 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = 3000;
-const es_address = "localhost"
-const es_port = "9200"
-const es_index = "products"
-const es_client = new Client({ node: `http://${es_address}:${es_port}` })
+const es_address = "localhost";
+const es_port = "9200";
+const es_index = "products";
+// Client for elasticsearch node
+//
+const es_client = new Client({ node: `http://${es_address}:${es_port}` });
 
-// Routes
-app.get('/search/:term?', async (req, res) => {
+// Autocomplete endpoint
+//
+app.get("/search/:term?", async (req, res) => {
   let search_term = req.params.term;
   console.log(`GET /search/${search_term}`);
-  es_client.search({
-    index: es_index,
-    body: {
-      query: {
-        multi_match: {
-          query: search_term,
-          type: "bool_prefix",
-          fields: ["name", "name._2gram", "name._3gram"]
+  es_client.search(
+    // Query DSL
+    {
+      index: es_index,
+      body: {
+        query: {
+          multi_match: {
+            query: search_term,
+            type: "bool_prefix",
+            fields: ["name", "name._2gram", "name._3gram"],
+          },
+        },
+      },
+    },
+    // Callback
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.end();
+      } else {
+        console.log(`statusCode: ${result.statusCode}`);
+        console.log(result.body.hits.total);
+        res.set("Content-Type", "application/json");
+        let hits = new Array();
+        for (let hit of result.body.hits.hits) {
+          hits.push(hit._source.name);
         }
+        res.send(JSON.stringify(hits)); // Return an array of product names
       }
     }
-  }, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    }
-    else {
-      console.log(`statusCode: ${result.statusCode}`);
-      console.log(result.body.hits.total);
-      res.set('Content-Type', 'application/json');
-      let hits = new Array();
-      for (let hit of result.body.hits.hits) {
-        hits.push(hit._source.name);
-      }
-      res.send(JSON.stringify(hits));
-    }
-  });
+  );
 });
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve static html and javascript
+//
+app.use(express.static(path.join(__dirname, "public")));
 
 //Makes the app listen to port 3000
 app.listen(port, () => console.log(`App listening to port ${port}`));
